9e2c91c0abddf60617c1f8e16c3d0202df0db82e
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-22 15:12:05 +0600

    404 page created

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PageNotFound() {
+  return (
+    <div className="text-center p-4 bg-orange-700 text-white text-3xl">404 Page Not Found</div>
+  )
+}
