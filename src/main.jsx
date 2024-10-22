ce5e536dafadf656bc9489dc1268259c236be01b
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-22 15:10:56 +0600

    queryClient added from tanstack react query

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,24 @@ import { Provider } from 'react-redux'
 import { store, persistor } from './store/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor} >
-        <App />
-      </PersistGate>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor} >
+          <App />
+        </PersistGate>
+      </Provider>
+    </QueryClientProvider>
   </BrowserRouter>,
 )
