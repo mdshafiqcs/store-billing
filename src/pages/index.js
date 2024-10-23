021bf2038fe9684f1ea3a92fd41815b6fbc5a288
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-23 12:50:05 +0600

    pages imported to the index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import AuthPage from "./AuthPage";
 import PageNotFound from "./PageNotFound";
+import DashboardPage from "./DashboardPage";
+import HomePage from "./HomePage";
 
 
 
 export {
   AuthPage, 
   PageNotFound,
+  DashboardPage,
+  HomePage,
 }
