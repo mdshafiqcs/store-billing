b1f852d5663a696df403766dd73ba1ab139cefdd
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-23 12:50:56 +0600

    conditionaly rendering home page and authpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { AuthPage } from "./pages/index.js"
+import { useSelector } from "react-redux";
+import { AuthPage, HomePage } from "./pages/index.js"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
+
 function App() {
 
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
   return (
-    <>
-      <AuthPage />
+    <> {
+      isLoggedIn ? <HomePage /> : <AuthPage />
+    }
+
       <ToastContainer />
     </>
   )
