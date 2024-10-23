a8a50903e0cdd01332af200d080df0e2a995e31b
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-23 12:48:00 +0600

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import LogoutButton from './LogoutButton';
+
+export default function Navbar() {
+  
+
+  return (
+    <div className=' bg-white border-b border-slate-200 shadow'>
+      <div className="mx-20 flex justify-between items-center py-4">
+        <div className="font-bold text-2xl text-slate-600 cursor-pointer">Store<span className="text-blue-500">X</span> </div>
+        <LogoutButton />
+        
+      </div>
+    </div>
+  )
+}
